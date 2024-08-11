f60bfb367715ad8da66ef0aeffe22f7f6ce0a211
Fabian Döz <fabian@doez.info>
2024-08-11 11:27:52 +0200

    some fun with css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {InfoBoxComponent} from "./info-box/info-box.component";
+import {NewBoxComponent} from "./new-box/new-box.component";
 
 @Component({
   selector: 'currywurst',
   standalone: true,
-  imports: [RouterOutlet, InfoBoxComponent],
+  imports: [RouterOutlet, InfoBoxComponent, RouterLink, NewBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
